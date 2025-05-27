277228138f263fa15e44e704767748671683714e
MbarrouxCwu <mickael.barroux@codewithus.fr>
2025-05-27 10:26:40 +0200

    Step 3 : add JWT token signature check

--- src/main/java/com/breizhcamp/demo/saloon/SaloonController.java
+++ src/main/java/com/breizhcamp/demo/saloon/SaloonController.java
@@ -2,33 +2,43 @@ package com.breizhcamp.demo.saloon;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @RestController
 @RequestMapping("/saloon")
 class SaloonController {
 
+    private final SecretKey secret;
+
+    private SaloonController(@Value("${jwt.secret-key}") final String secretKey) {
+        this.secret = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    }
+
     @GetMapping
     public String haveADrink(@RequestHeader(value = "Authorization", required = false) final String authorizationHeader) {
         try {
             final String token = extractToken(authorizationHeader);
 
-            final Jwt<Header, Claims> jwt = Jwts.parserBuilder()
+            final Jws<Claims> jws = Jwts.parserBuilder()
+                    .setSigningKey(this.secret)
                     .build()
-                    .parseClaimsJwt(token);
-            final Claims claims = jwt.getBody();
+                    .parseClaimsJws(token);
+            final Claims claims = jws.getBody();
 
             final Date exp = claims.getExpiration();
             if (exp == null || exp.before(new Date())) {
